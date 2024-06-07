97b1c070b576401cbca84454817df423b7679f19
Tạ Anh Quý-Intern <taquy2311@gmail.com>
2024-06-07 10:37:23 +0700

    Fix/TokenExpires

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import ProductList from "./component/products/ProductsList";
 import Layouts from "./Layout";
 import Home from "./component/home/Home";
 import ProductsAdd from "./component/products/ProductsAdd";
-import ProductEdit  from "./component/products/ProductEdit";
-import ServicesList  from "./component/services/ServicesList";
-import ServicesAdd  from "./component/services/ServicesAdd";
-import ServicesEdit  from "./component/services/ServicesEdit";
+import ProductEdit from "./component/products/ProductEdit";
+import ServicesList from "./component/services/ServicesList";
+import ServicesAdd from "./component/services/ServicesAdd";
+import ServicesEdit from "./component/services/ServicesEdit";
 import Login from "./component/Auth/Login";
 import NotFound from "./component/NotFound";
 import UserList from "./component/User/UserList";
@@ -21,43 +21,45 @@ import TicketAdd from "./component/tickets/TicketAdd";
 import TicketDetail from "./component/tickets/TicketDetail";
 import { ToastContainer } from "react-toastify";
 import CustomersList from "./component/customers/CustomersList";
+import { AuthProvider } from "./Context/ContextAuth";
 
 const App = () => {
   return (
     <Router>
-      <ToastContainer/>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/resetpassword/:token" element={<ResetPassword />} />
-        <Route path="/changepassword" element={<ChangePassword />} />
+      <AuthProvider>
+        <ToastContainer />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/resetpassword/:token" element={<ResetPassword />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
 
-        <Route element={<PrivateRouter />}>
-          <Route element={<Layouts />}>
+          <Route element={<PrivateRouter />}>
+            <Route element={<Layouts />}>
+              <Route path="/" element={<Home />} />
 
-            <Route path="/" element={<Home />} />
+              <Route path="/product" element={<ProductList />} />
+              <Route path="/product/add" element={<ProductsAdd />} />
+              <Route path="/product/:id" element={<ProductEdit />} />
 
-            <Route path="/product" element={<ProductList />} />
-            <Route path="/product/add" element={<ProductsAdd />} />
-            <Route path="/product/:id" element={<ProductEdit />} />
+              <Route path="/services" element={<ServicesList />} />
+              <Route path="/services/add" element={<ServicesAdd />} />
+              <Route path="/services/:id" element={<ServicesEdit />} />
 
-            <Route path="/services" element={<ServicesList/>} />
-            <Route path="/services/add" element={<ServicesAdd/>} />
-            <Route path="/services/:id" element={<ServicesEdit />} />
+              <Route path="/user" element={<UserList />} />
+              <Route path="/user/add" element={<UserAdd />} />
+              <Route path="/user/:id" element={<UserEdit />} />
 
-            <Route path="/user" element={<UserList />} />
-            <Route path="/user/add" element={<UserAdd />} />
-            <Route path="/user/:id" element={<UserEdit />} />
+              <Route path="ticket" element={<TicketList/>}/>
+              <Route path="ticket/add" element={<TicketAdd/>}/>
+              <Route path="ticket/:id" element={<TicketDetail/>}/>
 
-            <Route path="ticket" element= {<TicketList/>}/>
-            <Route path="ticket/add" element= {<TicketAdd/>}/>
-            <Route path="ticket/:id" element= {<TicketDetail/>}/>
-
-            <Route path="customers" element= {<CustomersList/>}/>
+              <Route path="customers" element={<CustomersList/>}/>
+            </Route>
           </Route>
-        </Route>
-        <Route path="/error" element={<NotFound />} />
-      </Routes>
+          <Route path="/error" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
